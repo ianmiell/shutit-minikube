f5ae6646e9789c144d6177dbb821c4176dd1ac77
Ian Miell <ian.miell@gmail.com>
2017-02-18 14:41:51 +0000

    simpler

--- shutit_minikube.py
+++ shutit_minikube.py
@@ -9,93 +9,25 @@ class shutit_minikube(ShutItModule):
 
 
 	def build(self, shutit):
-		vagrant_image = shutit.cfg[self.module_id]['vagrant_image']
-		vagrant_provider = shutit.cfg[self.module_id]['vagrant_provider']
-		gui = shutit.cfg[self.module_id]['gui']
-		memory = shutit.cfg[self.module_id]['memory']
-		shutit.cfg[self.module_id]['vagrant_run_dir'] = os.path.dirname(os.path.abspath(inspect.getsourcefile(lambda:0))) + '/vagrant_run'
-		run_dir = shutit.cfg[self.module_id]['vagrant_run_dir']
-		module_name = 'shutit_minikube_' + ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
-		this_vagrant_run_dir = run_dir + '/' + module_name
-		shutit.cfg[self.module_id]['this_vagrant_run_dir'] = this_vagrant_run_dir
-		shutit.send(' command rm -rf ' + this_vagrant_run_dir + ' && command mkdir -p ' + this_vagrant_run_dir + ' && command cd ' + this_vagrant_run_dir)
-		shutit.send('command rm -rf ' + this_vagrant_run_dir + ' && command mkdir -p ' + this_vagrant_run_dir + ' && command cd ' + this_vagrant_run_dir)
-		if shutit.send_and_get_output('vagrant plugin list | grep landrush') == '':
-			shutit.send('vagrant plugin install landrush')
-		shutit.send('vagrant init ' + vagrant_image)
-		shutit.send_file(this_vagrant_run_dir + '/Vagrantfile','''Vagrant.configure("2") do |config|
-  config.landrush.enabled = true
-  config.vm.provider "virtualbox" do |vb|
-    vb.gui = ''' + gui + '''
-    vb.memory = "''' + memory + '''"
-  end
+		# https://kubernetes.io/docs/user-guide/prereqs/
 
-  config.vm.define "minikube1" do |minikube1|
-    minikube1.vm.box = ''' + '"' + vagrant_image + '"' + '''
-    minikube1.vm.hostname = "minikube1.vagrant.test"
-    config.vm.provider :virtualbox do |vb|
-      vb.name = "shutit_minikube_1"
-    end
-  end
-end''')
-		pw = shutit.get_env_pass()
-		try:
-			shutit.multisend('vagrant up --provider ' + shutit.cfg['shutit-library.virtualization.virtualization.virtualization']['virt_method'] + " minikube1",{'assword for':pw,'assword:':pw},timeout=99999)
-		except NameError:
-			shutit.multisend('vagrant up minikube1',{'assword for':pw,'assword:':pw},timeout=99999)
-		if shutit.send_and_get_output("""vagrant status | grep -w ^minikube1 | awk '{print $2}'""") != 'running':
-			shutit.pause_point("machine: minikube1 appears not to have come up cleanly")
-
-
-		# machines is a dict of dicts containing information about each machine for you to use.
-		machines = {}
-		machines.update({'minikube1':{'fqdn':'minikube1.vagrant.test'}})
-		ip = shutit.send_and_get_output('''vagrant landrush ls 2> /dev/null | grep -w ^''' + machines['minikube1']['fqdn'] + ''' | awk '{print $2}' ''')
-		machines.get('minikube1').update({'ip':ip})
-
-		for machine in sorted(machines.keys()):
-			shutit.login(command='vagrant ssh ' + machine)
-			shutit.login(command='sudo su -',password='vagrant')
-			root_password = 'root'
-			shutit.install('net-tools') # netstat needed
-			if not shutit.command_available('host'):
-				shutit.install('bind-utils') # host needed
-			# Workaround for docker networking issues + landrush.
-			shutit.install('docker')
-			shutit.insert_text('Environment=GODEBUG=netdns=cgo','/lib/systemd/system/docker.service',pattern='.Service.')
-			shutit.send('mkdir -p /etc/docker',note='Create the docker config folder')
-			shutit.send_file('/etc/docker/daemon.json',"""{
-  "dns": ["8.8.8.8"]
-}""",note='Use the google dns server rather than the vagrant one. Change to the value you want if this does not work, eg if google dns is blocked.')
-			shutit.send('systemctl restart docker')
-			shutit.multisend('passwd',{'assword:':root_password})
-			shutit.send("""sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config""")
-			shutit.send("""sed -i 's/.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config""")
-			shutit.send('service ssh restart || systemctl restart sshd')
-			shutit.multisend('ssh-keygen',{'Enter':'','verwrite':'n'})
-			shutit.logout()
-			shutit.logout()
-		for machine in sorted(machines.keys()):
-			shutit.login(command='vagrant ssh ' + machine)
-			shutit.login(command='sudo su -',password='vagrant')
-			for copy_to_machine in machines:
-				for item in ('fqdn','ip'):
-					shutit.multisend('ssh-copy-id root@' + machines[copy_to_machine][item],{'assword:':root_password,'ontinue conn':'yes'})
-			shutit.logout()
-			shutit.logout()
-		shutit.login(command='vagrant ssh ' + sorted(machines.keys())[0])
-		shutit.login(command='sudo su -',password='vagrant')
+		# ASSUMING LINUX FOR NOW
+		# OS X
+		# curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl
+		# Linux
+		shutit.send('curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl')
+		shutit.send('chmod +x kubectl')
+		shutit.send('cp kubectl /usr/local/bin/kubectl')
+		# Windows
+		#curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/windows/amd64/kubectl.exe
 		shutit.send('curl https://storage.googleapis.com/minikube/releases/v0.16.0/minikube-linux-amd64 > minikube')
 		shutit.send('chmod +x minikube')
+		shutit.send('./minikube start')
+		shutit.send('kubectl run hello-minikube --image=gcr.io/google_containers/echoserver:1.4 --port=8080')
+		shutit.send('kubectl expose deployment hello-minikube --type=NodePort')
+		shutit.send('kubectl get pod')
+		shutit.send('curl $(minikube service hello-minikube --url)')
 		shutit.pause_point('')
-		shutit.logout()
-		shutit.logout()
-		shutit.log('''Vagrantfile created in: ''' + this_vagrant_run_dir,add_final_message=True,level=logging.DEBUG)
-		shutit.log('''Run:
-
-	cd ''' + this_vagrant_run_dir + ''' && vagrant status && vagrant landrush ls
-
-To get a picture of what has been set up.''',add_final_message=True,level=logging.DEBUG)
 		return True
 
 
@@ -153,5 +85,5 @@ def module():
 		description='',
 		maintainer='',
 		delivery_methods=['bash'],
-		depends=['shutit.tk.setup','shutit-library.virtualization.virtualization.virtualization','tk.shutit.vagrant.vagrant.vagrant']
+		depends=['shutit.tk.setup','shutit-library.virtualization.virtualization.virtualization']
 	)
