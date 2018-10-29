70076907d9c6b6665530397e6d636533448e416d
Ian Miell <ian.miell@gmail.com>
2018-10-29 12:57:51 +0000

    52

--- minikube_library/istio.py
+++ minikube_library/istio.py
@@ -12,4 +12,6 @@ def do_istio(s):
 	s.send('cd istio-1.0.3')
 	s.send('./bin/istioctl version')
 	s.send('kubectl create -f install/kubernetes/istio-demo.yaml')
-	s.pause_point('p39')
+	s.send_until('kubectl get pod -n istio-system | grep -v ^NAME | grep -v Running | grep -v Completed | wc -l','0')
+	s.send("kubectl run -i --rm --restart=Never dummy --image=tutum/curl:alpine -n istio-system --command -- curl -v 'http://istio-pilot.istio-system:8080/v1/registration'")
+	s.pause_point('p52')
