a40f528bbc02c113f18fc968769a58fbd67fce89
Ian Miell <ian.miell@gmail.com>
2018-10-31 07:29:57 +0000

    merged

--- minikube_library/istio.py
+++ minikube_library/istio.py
@@ -12,7 +12,7 @@ def do_istio(s, version):
 		s.send('wget -qO- https://github.com/istio/istio/releases/download/' + version + '/istio-' + version + '-osx.tar.gz | tar -zxvf -')
 	s.send('cd istio-' + version)
 	s.send('./bin/istioctl version')
-	s.send('PATH=$(pwd)/bin/:${PATH}')
+	s.send('PATH=$(pwd)/bin:${PATH}')
 	# Errors at end here (no matter which version of istio?)
 	s.send('kubectl create -f install/kubernetes/istio-demo.yaml || true')
 	s.send_until('kubectl get pod -n istio-system | grep -v ^NAME | grep -v Running | grep -v Completed | wc -l','0')
@@ -20,9 +20,11 @@ def do_istio(s, version):
 	s.send('kubectl create namespace istioinaction')
 	s.send('kubectl config set-context $(kubectl config current-context) --namespace=istioinaction')
 	s.send('cd ../book-source-code')
+	# Deploy catalog app
 	s.send('istioctl create -f <(istioctl kube-inject -f install/catalog-service/catalog-all.yaml)')
 	s.send_until('kubectl get pod -n istio-system | grep catalog | grep -v ^NAME | grep -v Running | grep -v Completed | wc -l','0')
 	s.send("kubectl run -i --rm --restart=Never dummy --image=byrnedo/alpine-curl --command -- sh -c 'curl -s catalog:8080/api/catalog'")
+	# Deploy API gateway service
 	s.send('istioctl create -f <(istioctl kube-inject -f install/catalog-service/apigateway-all.yaml)')
 	s.send_until('kubectl get pod -n istio-system | grep apigateway | grep -v ^NAME | grep -v Running | grep -v Completed | wc -l','0')
 	s.send("kubectl run -i --rm --restart=Never dummy --image=byrnedo/alpine-curl --command -- sh -c 'curl -s apigateway:8080/api/products'")
