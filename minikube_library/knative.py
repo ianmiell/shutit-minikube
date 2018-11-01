aa359b6d5ef1667541b12edf53ce7c59474dab96
Ian Miell <ian.miell@gmail.com>
2018-11-01 13:55:16 +0000

    latest

--- minikube_library/knative.py
+++ minikube_library/knative.py
@@ -2,6 +2,7 @@ def do_knative(s):
 	# From: https://github.com/knative/docs/blob/master/install/Knative-with-Minikube.md
 	s.send("curl -L https://raw.githubusercontent.com/knative/serving/v0.1.1/third_party/istio-0.8.0/istio.yaml | sed 's/LoadBalancer/NodePort/' | kubectl apply --filename -")
 	s.send('kubectl label namespace default istio-injection=enabled')
+	# statsd fails for some reason?
 	s.send_until('kubectl get pod -n istio-system | grep -v ^NAME | grep -v Running | grep -v Completed | wc -l','0',cadence=20)
 	s.send('''curl -L https://github.com/knative/serving/releases/download/v0.1.1/release-lite.yaml | sed 's/LoadBalancer/NodePort/' | kubectl apply --filename -''')
 	s.send_until('kubectl get pod -n knative-serving | grep -v ^NAME | grep -v Running | wc -l','0',cadence=20)
