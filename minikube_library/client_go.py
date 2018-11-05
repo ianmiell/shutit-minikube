add7b33a05c8fd6d9501d1e9f5975ed581f9ca9b
Ian Miell <ian.miell@gmail.com>
2018-11-05 19:55:27 +0000

    tabs

--- minikube_library/client_go.py
+++ minikube_library/client_go.py
@@ -23,31 +23,31 @@ END''')
 package main
 
 import (
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/kubernetes"
-	"log"
-	"fmt"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/client-go/tools/clientcmd"
+    "k8s.io/client-go/kubernetes"
+    "log"
+    "fmt"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func main() {
-	kubeconfig = "/home/imiell/.kube/config"
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		log.Fatal(err)
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Fatal(err)
-	}
-	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-	for _, pod := rangepods.Items {
-		fmt.Printf("Pod name %s\n", pod.GetName())
-	}
+    kubeconfig = "/home/imiell/.kube/config"
+    config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+    if err != nil {
+        log.Fatal(err)
+    }
+    clientset, err := kubernetes.NewForConfig(config)
+    if err != nil {
+        log.Fatal(err)
+    }
+    pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+    if err != nil {
+        panic(err.Error())
+    }
+    fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+    for _, pod := range pods.Items {
+        fmt.Printf("Pod name %s\n", pod.GetName())
+    }
 }
 END''')
 	s.send('go build')
