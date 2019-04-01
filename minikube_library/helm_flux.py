53086d7502a3f08a5ef6b56566f29f541ffa1374
Ian Miell <ian.miell@gmail.com>
2019-04-01 21:56:33 +0100

    back

--- minikube_library/helm_flux.py
+++ minikube_library/helm_flux.py
@@ -117,9 +117,7 @@ subjects:
 
 	# FLUX GLOBAL (installs tenant helm and flux)
 	s.send('helm repo add weaveworks https://weaveworks.github.io/flux',note='Add helm repo for flux')
-	#s.send('kubectl apply -f https://raw.githubusercontent.com/weaveworks/flux/master/deploy-helm/flux-helm-release-crd.yaml',note='create CRD for flux')
-	# Stick to version 1.10.1, as 1.11.0 is broken.
-	s.send('kubectl apply -f https://github.com/weaveworks/flux/blob/1.10.1/deploy-helm/flux-helm-release-crd.yaml',note='create CRD for flux')
+	s.send('kubectl apply -f https://raw.githubusercontent.com/weaveworks/flux/master/deploy-helm/flux-helm-release-crd.yaml',note='create CRD for flux')
 
 	s.send('helm delete --purge flux || true',note='Delete any pre-existing helm install, as per https://github.com/helm/helm/issues/3208')
 	s.send('sleep 120',note='Wait until flux ready set up')
