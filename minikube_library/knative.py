1d7642b7f37f7bac95ae5b3cd0bc9b6c93f8036f
ian <ian.miell@gmail.com>
2019-08-07 15:44:52 +0100

    example

--- minikube_library/knative.py
+++ minikube_library/knative.py
@@ -16,3 +16,139 @@ def do_knative(s):
 	s.send_until('kubectl get pods --namespace knative-eventing | grep -v ^NAME | grep -v Running | wc -l','0', cadence=20)
 	s.send_until('kubectl get pods --namespace knative-monitoring | grep -v ^NAME | grep -v Running | wc -l','0', cadence=20)
 
+
+def do_kubernetes_eventing(s):
+	# https://knative.dev/docs/eventing/samples/kubernetes-event-source/
+	s.send('kubectl create namespace k8s-events')
+	s.send('kubectl config set-context --current --namespace=k8s-events')
+	s.send('kubectl label namespace k8s-events knative-eventing-injection=enabled')
+	# Create a Service Account that the ApiServerSource runs as.
+	# The ApiServerSource watches for Kubernetes events and forwards them to the Knative Eventing Broker.
+	# Create a file named serviceaccount.yaml and copy the code block below into it.
+	s.send_file('serviceaccount.yaml', '''apiVersion: serving.knative.dev/v1alpha1
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: events-sa
+  namespace: k8s-events
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: event-watcher
+rules:
+  - apiGroups:
+      - ""
+    resources:
+      - events
+    verbs:
+      - get
+      - list
+      - watch
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: k8s-ra-event-watcher
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: event-watcher
+subjects:
+  - kind: ServiceAccount
+    name: events-sa
+    namespace: k8s-events''')
+	s.send('kubectl create -f serviceaccount.yaml')
+	# In order to receive events, you have to create a concrete Event Source for a specific namespace.
+	# Create a file named k8s-events.yaml and copy the code block below into it
+	s.send_file('k8s-events.yaml','''apiVersion: sources.eventing.knative.dev/v1alpha1
+kind: ApiServerSource
+metadata:
+  name: testevents
+  namespace: k8s-events
+spec:
+  serviceAccountName: events-sa
+  mode: Resource
+  resources:
+    - apiVersion: v1
+      kind: Event
+  sink:
+    apiVersion: eventing.knative.dev/v1alpha1
+    kind: Broker
+    name: k8s-events''')
+	s.send('kubectl create -f k8s-events.yaml')
+	# In order to check the ApiServerSource is fully working, we will create a simple
+	# Knative Service that dumps incoming messages to its log and creates a
+	# Trigger from the Broker to that Knative Service.
+	# Create a file named trigger.yaml and copy the code block below into it.
+	# If the deployed ApiServerSource is pointing at a Broker other than k8s-events, modify trigger.yaml by adding spec.broker with the Broker’s name.
+	s.send_file('trigger.yaml','''apiVersion: eventing.knative.dev/v1alpha1
+kind: Trigger
+metadata:
+  name: testevents-trigger
+  namespace: k8s-events
+spec:
+  subscriber:
+    ref:
+      apiVersion: serving.knative.dev/v1alpha1
+      kind: Service
+      name: event-display
+---
+# This is a very simple Knative Service that writes the input request to its log.
+apiVersion: serving.knative.dev/v1alpha1
+kind: Service
+metadata:
+  name: event-display
+  namespace: k8s-events
+spec:
+  template:
+    spec:
+      containers:
+        - # This corresponds to
+          # https://github.com/knative/eventing-contrib/blob/release-0.5/cmd/event_display/main.go
+          image: gcr.io/knative-releases/github.com/knative/eventing-sources/cmd/event_display@sha256:bf45b3eb1e7fc4cb63d6a5a6416cf696295484a7662e0cf9ccdf5c080542c21d''')
+	s.send('kubectl create -f trigger.yaml')
+	# Create events by launching a pod in the k8s-events namespace. Create a busybox container and immediately delete it.
+	s.send('kubectl run busybox --image=busybox --restart=Never -- ls')
+	s.send('kubectl delete pod busybox')
+	s.send('kubectl get pods')
+	s.send('kubectl logs -l serving.knative.dev/service=event-display -c user-container')
+	s.pause_point('''
+
+''')
+	s.send('kubectl --namespace default delete --filename serviceaccount.yaml')
+	s.send('kubectl --namespace default delete --filename k8s-events.yaml')
+	s.send('kubectl --namespace default delete --filename trigger.yaml')
+
+
+def do_cron_eventing(s):
+	# https://knative.dev/docs/eventing/samples/cronjob-source/
+	s.send('service.yaml', '''apiVersion: serving.knative.dev/v1alpha1
+kind: Service
+metadata:
+  name: event-display
+spec:
+  template:
+    spec:
+      containers:
+        - image: gcr.io/knative-releases/github.com/knative/eventing-sources/cmd/event_display''')
+	s.send('kubectl apply -f service.yaml')
+	s.send_file('cronjob-source.yaml', '''apiVersion: sources.eventing.knative.dev/v1alpha1
+kind: CronJobSource
+metadata:
+  name: test-cronjob-source
+spec:
+  schedule: "*/2 * * * *"
+  data: '{"message": "Hello world!"}'
+  sink:
+    apiVersion: serving.knative.dev/v1alpha1
+    kind: Service
+    name: event-display''')
+	s.send('kubectl apply -f cronjob-source.yaml')
+	s.pause_point('''Every two minutes a message should be sent to the event-display service:
+
+kubectl logs -l serving.knative.dev/service=event-display -c user-container --since=10m
+
+''')
+	s.send('kubectl delete --filename cronjob-source.yaml')
+	s.send('kubectl delete --filename service.yaml')
