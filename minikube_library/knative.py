61eb621c18093961fad178a7e4d29807eda3430c
ian <ian.miell@gmail.com>
2019-08-07 17:00:51 +0100

    message

--- minikube_library/knative.py
+++ minikube_library/knative.py
@@ -1,24 +1,23 @@
 def do_knative(s):
 	# https://github.com/knative/docs/blob/master/docs/install/Knative-with-Minikube.md
-	s.send('''kubectl apply --filename https://raw.githubusercontent.com/knative/serving/v0.7.0/third_party/istio-1.1.7/istio-crds.yaml''')
-	s.send('''sleep 30''')
-	s.send('''curl -L https://raw.githubusercontent.com/knative/serving/v0.7.0/third_party/istio-1.1.7/istio.yaml | sed 's/LoadBalancer/NodePort/'   | kubectl apply --filename -''')
-	s.send('''sleep 10''')
-	# Label the default namespace with istio-injection=enabled.
-	s.send('kubectl label --overwrite namespace default istio-injection=enabled')
+	s.send('''kubectl apply --filename https://raw.githubusercontent.com/knative/serving/v0.5.2/third_party/istio-1.0.7/istio-crds.yaml''')
+	s.send('''curl -L https://raw.githubusercontent.com/knative/serving/v0.5.2/third_party/istio-1.0.7/istio.yaml | sed 's/LoadBalancer/NodePort/' | kubectl apply --filename - ''')
+	s.send('kubectl label namespace default istio-injection=enabled')
 	s.send_until('kubectl get pod -n istio-system | grep -v ^NAME | grep -v Running | grep -v Completed | wc -l','0',cadence=20)
 	s.send('''sleep 10''')
-	s.send('kubectl apply --selector knative.dev/crd-install=true --filename https://github.com/knative/serving/releases/download/v0.7.0/serving.yaml --filename https://github.com/knative/build/releases/download/v0.7.0/build.yaml --filename https://github.com/knative/eventing/releases/download/v0.7.0/release.yaml --filename https://github.com/knative/serving/releases/download/v0.7.0/monitoring.yaml')
+	s.send('''kubectl apply --selector knative.dev/crd-install=true --filename https://github.com/knative/serving/releases/download/v0.7.0/serving.yaml --filename https://github.com/knative/build/releases/download/v0.7.0/build.yaml --filename https://github.com/knative/eventing/releases/download/v0.7.0/release.yaml --filename https://github.com/knative/serving/releases/download/v0.7.0/monitoring.yaml''')
 	s.send('''sleep 10''')
-	# Run twice so that error is not seen again.
-	s.send('kubectl apply --selector knative.dev/crd-install=true --filename https://github.com/knative/serving/releases/download/v0.7.0/serving.yaml --filename https://github.com/knative/build/releases/download/v0.7.0/build.yaml --filename https://github.com/knative/eventing/releases/download/v0.7.0/release.yaml --filename https://github.com/knative/serving/releases/download/v0.7.0/monitoring.yaml')
-	s.send('''sleep 30''')
-	# Then run again.
-	s.send('kubectl apply --filename https://github.com/knative/serving/releases/download/v0.7.0/serving.yaml --selector networking.knative.dev/certificate-provider!=cert-manager --filename https://github.com/knative/build/releases/download/v0.7.0/build.yaml --filename https://github.com/knative/eventing/releases/download/v0.7.0/release.yaml --filename https://github.com/knative/serving/releases/download/v0.7.0/monitoring.yaml')
+	# Run twice to ensure we don't get the error.
+	s.send('''kubectl apply --selector knative.dev/crd-install=true --filename https://github.com/knative/serving/releases/download/v0.7.0/serving.yaml --filename https://github.com/knative/build/releases/download/v0.7.0/build.yaml --filename https://github.com/knative/eventing/releases/download/v0.7.0/release.yaml --filename https://github.com/knative/serving/releases/download/v0.7.0/monitoring.yaml''')
+	s.send('''sleep 10''')
+	s.send('''kubectl apply --filename https://github.com/knative/serving/releases/download/v0.7.0/serving.yaml --selector networking.knative.dev/certificate-provider!=cert-manager --filename https://github.com/knative/build/releases/download/v0.7.0/build.yaml --filename https://github.com/knative/eventing/releases/download/v0.7.0/release.yaml --filename https://github.com/knative/serving/releases/download/v0.7.0/monitoring.yaml''')
 	s.send_until('kubectl get pods --namespace knative-serving | grep -v ^NAME | grep -v Running | wc -l','0', cadence=20)
 	s.send_until('kubectl get pods --namespace knative-build | grep -v ^NAME | grep -v Running | wc -l','0', cadence=20)
 	s.send_until('kubectl get pods --namespace knative-eventing | grep -v ^NAME | grep -v Running | wc -l','0', cadence=20)
 	s.send_until('kubectl get pods --namespace knative-monitoring | grep -v ^NAME | grep -v Running | wc -l','0', cadence=20)
+	s.pause_point('ready')
+	do_kubernetes_eventing(s)
+	do_cron_eventing(s)
 
 
 def do_kubernetes_eventing(s):
@@ -117,8 +116,11 @@ spec:
 	s.send('kubectl run busybox --image=busybox --restart=Never -- ls')
 	s.send('kubectl delete pod busybox')
 	s.send('kubectl get pods')
-	s.send('kubectl logs -l serving.knative.dev/service=event-display -c user-container')
-	s.pause_point('''
+	s.pause_point('''See events by running:
+
+kubectl logs -l serving.knative.dev/service=event-display -c user-container -f
+
+Continue by hitting CTRL-]
 
 ''')
 	s.send('kubectl --namespace default delete --filename serviceaccount.yaml')
